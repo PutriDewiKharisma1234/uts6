5ff53bca44ed881f47ae857a912da696323f7988
PutriDewiKharisma1234 <putrid33090601@gmail.com>
2024-05-04 15:52:19 +0700

    penambahan models

--- Penjualan/PenjualanBarang/models.py
+++ Penjualan/PenjualanBarang/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Produk(models.Model):
+    nama = models.CharField(max_length=100)
+    deskripsi = models.TextField()
+    harga = models.DecimalField(max_digits=10, decimal_places=2)
+    stok = models.IntegerField()
+
+    def __str__(self):
+        return self.nama
+    
+class Pelanggan(models.Model):
+    nama = models.CharField(max_length=100)
+    alamat = models.TextField()
+    nomor_telepon = models.CharField(max_length=15)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.nama
+    
+class Pesanan(models.Model):
+    produk = models.ForeignKey(Produk, on_delete=models.CASCADE)
+    pelanggan = models.ForeignKey(Pelanggan, on_delete=models.CASCADE)
+    jumlah = models.IntegerField()
+    total_harga = models.DecimalField(max_digits=10, decimal_places=2)
+    tanggal_pesanan = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.produk
+    
