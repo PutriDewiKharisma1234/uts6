22ff1e2b1db9241304fec0d31c519858757d5c72
PutriDewiKharisma1234 <putrid33090601@gmail.com>
2024-07-06 09:38:08 +0700

    uas

--- Penjualan/PenjualanBarang/models.py
+++ Penjualan/PenjualanBarang/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Produk(models.Model):
     nama = models.CharField(max_length=100)
     deskripsi = models.TextField()
-    harga = models.DecimalField(max_digits=10, decimal_places=2)
+    harga = models.DecimalField(max_digits=100, decimal_places=2)
     stok = models.IntegerField()
 
     def __str__(self):
@@ -12,7 +12,7 @@ class Produk(models.Model):
 class Pelanggan(models.Model):
     nama = models.CharField(max_length=100)
     alamat = models.TextField()
-    nomor_telepon = models.CharField(max_length=15)
+    nomor_telepon = models.CharField(max_length=100)
     email = models.EmailField()
 
     def __str__(self):
@@ -22,7 +22,7 @@ class Pesanan(models.Model):
     produk = models.ForeignKey(Produk, on_delete=models.CASCADE)
     pelanggan = models.ForeignKey(Pelanggan, on_delete=models.CASCADE)
     jumlah = models.IntegerField()
-    total_harga = models.DecimalField(max_digits=10, decimal_places=2)
+    total_harga = models.DecimalField(max_digits=100, decimal_places=2)
     tanggal_pesanan = models.DateField(auto_now_add=True)
 
     def __str__(self):
